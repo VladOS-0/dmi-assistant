b8a273aa2bc6a0000579155eaaae0b617402e4fe
Vladislav Stolyarchuk <vladstolyarchuk7@gmail.com>
2025-11-05 18:17:00 +0300

    Formatting fixes

--- build.rs
+++ build.rs
@@ -4,7 +4,8 @@ use winresource::WindowsResource;
 
 pub fn main() -> io::Result<()> {
     println!("cargo::rerun-if-changed=assets/fonts/fontello.toml");
-    iced_fontello::build("assets/fonts/fontello.toml").expect("Build fontello font");
+    iced_fontello::build("assets/fonts/fontello.toml")
+        .expect("Build fontello font");
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
         WindowsResource::new()
             .set_icon("assets/images/icon.ico")
